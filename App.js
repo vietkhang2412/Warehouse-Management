3314b6959ce4b0d4fc04ff492160c5c5acf25b7b
ChinhCao <chinhr.164@gmail.com>
2023-05-20 13:00:54 +0700

    test git

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Anh Chỉnh đã ở đây</Text>
+      <Text>Test GIT</Text>
       <StatusBar style="auto" />
     </View>
   );
